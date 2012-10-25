f2dabeec0c662402bf80245159e5ca7bf22935dd
Brad Kahl <bkahl1984@gmail.com>
2012-10-25 01:39:10 -0400

    fixed plugin so data loads dynmaically through quote plugin.

--- js/jquery.quotePlugin.js
+++ js/jquery.quotePlugin.js
@@ -3,41 +3,45 @@
  *	Date:	10.24.12
  *	Description: creates a dynamic quote that one can postion absolutely.
  **************************************************************************/
-
-
-	<div class="quote">
-		<!-- <div class="top"></div>
-		<div class="content">
-			
-			<div class="message">
-				<div class="open-quote"></div>
-				<span>Sed ipsum arcu, cons</span>
-				<p>Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.</p>
-				<div class="close-quote"></div>
-			</div>
-			
-			<div class="from">~ Pilot contributor</div>
-			
-		</div>
-		<div class="bottom"></div> -->
-	</div>
-	
 function createQuote(id, opts){
 	
-	var elements;
+	var elements, 
+		top = opts.layout.top || null,
+		right = opts.layout.right || null,
+		bottom = opts.layout.bottom || null,
+		left = opts.layout.left || null;
 	
-	elements = "<div class='top'></div>";
-	elements += "<div class='content'>";
-		elements += "<div class='message'>";
-			elements = "<div class='open-quote'></div>";
-			elements = "<span>"++"</span>";
-			elements = "<p>"++"</p>";
-			elements = "<div class='close-quote'></div>";
+	// creates markup
+	elements = "<div class='quote'>";
+		elements += "<div class='top'></div>";
+		elements += "<div class='content'>";
+			elements += "<div class='message'>";
+				elements += "<div class='open-quote'></div>";
+				elements += "<span>"+opts.firstLine+"</span>";
+				elements += "<p>"+opts.message+"</p>";
+				elements += "<div class='close-quote'></div>";
+			elements += "</div>";
+			elements += "<div class='from'>~ "+opts.fromWho+"</div>";
 		elements += "</div>";
+		elements += "<div class='bottom'></div>";
 	elements += "</div>";
-	elements = "<div class='bottom'></div>";
 		
-	$(id).append(elements);
+	// append markup to selector
+	$(id).append(elements).css('position','absolute');
+	
+	// relatively postion quote within container based upon user input in the ['layout'] property on the quote()
+	if(top){
+		$(id).css('top',top);
+	}
+	if(right){
+		$(id).css('right',right);
+	}
+	if(bottom){
+		$(id).css('bottom',bottom);
+	}
+	if(left){
+		$(id).css('left',left);
+	}
 	
 }
 
@@ -45,16 +49,18 @@ function createQuote(id, opts){
     $.fn.quote = function(options){
         var id = $(this), opts;
 
+		// default quote used if user doesn't input their own data
         $.fn.quote.defaults = {
-			message: 'Sed ipsum arcu, cons Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.',
-			from: 'Pilot contributor'
+			layout: { top: 0, left: 0 },
+			firstLine: 'Sed ipsum arcu, cons',
+			message: 'Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.Proin quis vehicula nunc. Quisque vehicula enim ac diam euismod et pellentesque eros lacinia. Proin tempor hendrerit nunc non adipiscing. Quisque in condimentu magna.',
+			fromWho: 'Pilot contributor'
         };
 
         opts = $.extend({}, $.fn.quote.defaults, options);
 
-        //createQuote(id, opts);
+        createQuote(id, opts);
     
         return this;    
     };
-
 }(jQuery));
